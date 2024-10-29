ea85799147cb509d71fefec2f5a78c6bd685c90d
Nehir Ozden <nehirlaraozden@gmail.com>
2024-10-29 09:19:44 -0400

    the mouse click does shoot and some enemy work

--- Team10_Unity_Final/Assets/Nate/EnemyMoveHit.cs
+++ Team10_Unity_Final/Assets/Nate/EnemyMoveHit.cs
@@ -78,3 +78,120 @@ public class EnemyMoveHit : MonoBehaviour {
               Gizmos.DrawWireSphere(transform.position, attackRange);
        }
 }
+
+
+// This is what I tried working on 
+
+// using System.Collections.Generic;
+// using System.Collections;
+// using UnityEngine;
+
+// public class EnemyMoveHit : MonoBehaviour {
+
+//        public Animator anim;
+//        public Rigidbody2D rb2D;
+//        public float speed = 4f;
+//        private Transform target;
+//        public int damage = 10;
+
+//        public int EnemyLives = 3;
+//       // private GameHandler gameHandler;   **** WHEN WE SET UP GAMEHANDLEr
+
+//        public float attackRange = 10;
+//        public bool isAttacking = false;
+//        private float scaleX;
+
+//        private Renderer rend; // Declare the Renderer variable
+
+//        public float knockBackForce = 0f;
+
+//        void Start () {
+//               anim = GetComponentInChildren<Animator> ();
+//               rb2D = GetComponent<Rigidbody2D> ();
+//               scaleX = gameObject.transform.localScale.x;
+//               rend = GetComponentInChildren<Renderer>(); // Initialize the Renderer
+
+//               if (GameObject.FindGameObjectWithTag ("Player") != null) {
+//                      target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+//               }
+
+//             //   if (GameObject.FindWithTag ("GameHandler") != null) {    **** WHEN WE SET UP GAMEHANDLER
+//             //       gameHandler = GameObject.FindWithTag ("GameHandler").GetComponent<GameHandler> ();
+//             //   }
+//        }
+
+//        void Update () {
+//               float DistToPlayer = Vector3.Distance(transform.position, target.position);
+
+//               if ((target != null) && (DistToPlayer <= attackRange)){
+//                      transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+//                     //anim.SetBool("Walk", true);
+//                     //flip enemy to face player direction. Wrong direction? Swap the * -1.
+//                     if (target.position.x > gameObject.transform.position.x){
+//                                    gameObject.transform.localScale = new Vector2(scaleX, gameObject.transform.localScale.y);
+//                     } else {
+//                                     gameObject.transform.localScale = new Vector2(scaleX * -1, gameObject.transform.localScale.y);
+//                     }
+//               }
+//                //else { anim.SetBool("Walk", false);}
+//        }
+
+//        public void OnCollisionEnter2D(Collision2D other){
+//               if (other.gameObject.tag == "Player") {
+//                      isAttacking = true;
+//                      anim.SetBool("Attack", true); //
+//                    //  gameHandler.playerGetHit(damage);   *** ADD BACK WHEN WE HAVE GAME HANDLER
+//                      rend.material.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);//
+//                      //StartCoroutine(HitEnemy());//
+//                      TakeDamage(1);
+
+//                      //Tell the player to STOP getting knocked back before getting knocked back:
+//                      other.gameObject.GetComponent<Player_EndKnockBack>().EndKnockBack();
+//                      //Add force to the player, pushing them back without teleporting:
+//                     Rigidbody2D pushRB = other.gameObject.GetComponent<Rigidbody2D>();
+//                     Vector2 moveDirectionPush = rb2D.transform.position - other.transform.position;
+//                     pushRB.AddForce(moveDirectionPush.normalized * knockBackForce * - 1f, ForceMode2D.Impulse);
+//               }
+//        }
+
+//        public void OnCollisionExit2D(Collision2D other){
+//               if (other.gameObject.tag == "Player") {
+//                      isAttacking = false;
+//                      anim.SetBool("Attack", false); //
+//               }
+//        }
+
+//        public void TakeDamage(int damage) {
+//               EnemyLives -= damage;
+//               Debug.Log("Enemy hit! Lives remaining: " + EnemyLives);
+
+//               // Change color to indicate hit
+//               if (rend != null) {
+//               rend.material.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);
+//        }
+
+//        if (EnemyLives <= 0) {
+//               Die();
+//               } else {
+//               StartCoroutine(ResetColor());
+//               }
+//        }
+
+//        IEnumerator ResetColor() {
+//               yield return new WaitForSeconds(0.5f);
+//               if (rend != null) {
+//               rend.material.color = Color.white;
+//               }
+//        }
+
+//        void Die() {
+//               Debug.Log("Enemy defeated!");
+//               Destroy(gameObject); // Remove the enemy from the scene
+//        }
+
+
+//        //DISPLAY the range of enemy's attack when selected in the Editor
+//        void OnDrawGizmosSelected(){
+//               Gizmos.DrawWireSphere(transform.position, attackRange);
+//        }
+// }
--- Team10_Unity_Final/Assets/Nehir/PlayerShooting.cs
+++ Team10_Unity_Final/Assets/Nehir/PlayerShooting.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    //public Animator animator;
     public Transform firePoint;
     public GameObject projectilePrefab;
     public float projectileSpeed = 10f;
@@ -17,36 +16,25 @@ public class PlayerShooting : MonoBehaviour
     }
 
     void Update(){
-        if (Time.time >= nextAttackTime){
-                //if (Input.GetKeyDown(KeyCode.Space))
-                if (Input.GetAxis("Attack") > 0){
-                    playerFire();
-                    nextAttackTime = Time.time + 1f / attackRate;
-                }
+        if (Time.time >= nextAttackTime) {
+            // Check for left mouse button click
+            if (Input.GetMouseButtonDown(0)) { // 0 is the left mouse button
+                playerFire();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
         }
     }
 
     void playerFire(){
-        // Before adding the mouse aim
-        //animator.SetTrigger ("Fire");
-        // Vector2 fwd = (firePoint.position - this.transform.position).normalized;
-        // GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
-        // projectile.GetComponent<Rigidbody2D>().AddForce(fwd * projectileSpeed, ForceMode2D.Impulse);
-
-        // Get the mouse position in world space
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0; 
+        mousePos.z = 0;
 
-        // Calculate the direction from the firePoint to the mouse position
         Vector2 direction = (mousePos - firePoint.position).normalized;
 
-        // Instantiate the projectile
         GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
 
-        // Add force to the projectile in the direction of the mouse position
         projectile.GetComponent<Rigidbody2D>().AddForce(direction * projectileSpeed, ForceMode2D.Impulse);
 
-        // Destroy the projectile after 5 seconds 
         Destroy(projectile, 5f);
     }
 }
