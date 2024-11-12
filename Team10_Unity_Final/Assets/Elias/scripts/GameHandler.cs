75a5a5d274a208cb097dba35b6c9e7dbe7c1fd95
ellavoorhees <ella.voorhees@tufts.edu>
2024-11-12 18:07:30 -0500

    Made Tutorial

--- Team10_Unity_Final/Assets/Elias/scripts/GameHandler.cs
+++ Team10_Unity_Final/Assets/Elias/scripts/GameHandler.cs
@@ -6,109 +6,105 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour {
 
-      private GameObject player;
-      public static int playerHealth = 100;
-      public int StartPlayerHealth = 100;
-      public GameObject healthText;
+    private GameObject player;
+    public static int playerHealth = 100;
+    public int StartPlayerHealth = 100;
+    public GameObject healthText;
 
-      public static int gotTokens = 0;
-      public GameObject tokensText;
+    public static int gotTokens = 0;
+    public GameObject tokensText;
 
-      public bool isDefending = false;
+    public bool isDefending = false;
 
-      public static bool stairCaseUnlocked = false;
-      //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
+    public static bool stairCaseUnlocked = false;
+    //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
 
-      private string sceneName;
-      public static string lastLevelDied;  //allows replaying the Level where you died
+    private string sceneName;
+    public static string lastLevelDied;  //allows replaying the Level where you died
 
-      void Start(){
-            player = GameObject.FindWithTag("Player");
-            sceneName = SceneManager.GetActiveScene().name;
-            //if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
-                  playerHealth = StartPlayerHealth;
-            //}
-            updateStatsDisplay();
-      }
+    void Start(){
+        player = GameObject.FindWithTag("Player");
+        sceneName = SceneManager.GetActiveScene().name;
+        playerHealth = StartPlayerHealth;
+        updateStatsDisplay();
+    }
 
-      public void playerGetTokens(int newTokens){
-            gotTokens += newTokens;
-            updateStatsDisplay();
-      }
+    public void playerGetTokens(int newTokens){
+        gotTokens += newTokens;
+        updateStatsDisplay();
+    }
 
-      public void playerGetHit(int damage){
-           if (isDefending == false){
-                  playerHealth -= damage;
-                  if (playerHealth >=0){
-                        updateStatsDisplay();
-                  }
-                  if (damage > 0){
-                        player.GetComponent<PlayerHurt>().playerHit();       //play GetHit animation
-                  }
+    public void playerGetHit(int damage){
+        if (!isDefending){
+            playerHealth -= damage;
+            if (playerHealth >= 0){
+                updateStatsDisplay();
             }
-
-           if (playerHealth > StartPlayerHealth){
-                  playerHealth = StartPlayerHealth;
-                  updateStatsDisplay();
+            if (damage > 0){
+                player.GetComponent<PlayerHurt>().playerHit();  //play GetHit animation
             }
+        }
 
-           if (playerHealth <= 0){
-                  playerHealth = 0;
-                  updateStatsDisplay();
-                  playerDies();
-            }
-      }
-
-      public void updateStatsDisplay(){
-            Text healthTextTemp = healthText.GetComponent<Text>();
-            healthTextTemp.text = "HEALTH: " + playerHealth;
-
-            Text tokensTextTemp = tokensText.GetComponent<Text>();
-            tokensTextTemp.text = "GOLD: " + gotTokens;
-      }
-
-      public void playerDies(){
-            player.GetComponent<PlayerHurt>().playerDead();       //play Death animation
-            lastLevelDied = sceneName;       //allows replaying the Level where you died
-            StartCoroutine(DeathPause());
-      }
-
-      IEnumerator DeathPause(){
-            player.GetComponent<playerMove>().isAlive = false;
-         //   player.GetComponent<PlayerJump>().isAlive = false;
-            yield return new WaitForSeconds(1.0f);
-            SceneManager.LoadScene("EndLose");
-      }
-
-      public void StartGame() {
-            SceneManager.LoadScene("Nate_test");
-      }
-
-      // Return to MainMenu
-      public void RestartGame() {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("MainMenu");
-             // Reset all static variables here, for new games:
+        if (playerHealth > StartPlayerHealth){
             playerHealth = StartPlayerHealth;
-      }
+            updateStatsDisplay();
+        }
 
-      // Replay the Level where you died
-      public void ReplayLastLevel() {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(lastLevelDied);
-             // Reset all static variables here, for new games:
-            playerHealth = StartPlayerHealth;
-      }
+        if (playerHealth <= 0){
+            playerHealth = 0;
+            updateStatsDisplay();
+            playerDies();
+        }
+    }
 
-      public void QuitGame() {
-                #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-                #else
-                Application.Quit();
-                #endif
-      }
+    public void updateStatsDisplay(){
+        Text healthTextTemp = healthText.GetComponent<Text>();
+        healthTextTemp.text = "HEALTH: " + playerHealth;
 
-      public void Credits() {
-            SceneManager.LoadScene("Credits");
-      }
+        Text tokensTextTemp = tokensText.GetComponent<Text>();
+        tokensTextTemp.text = "GOLD: " + gotTokens;
+    }
+
+    public void playerDies(){
+        player.GetComponent<PlayerHurt>().playerDead();  //play Death animation
+        lastLevelDied = sceneName;  //allows replaying the Level where you died
+        StartCoroutine(DeathPause());
+    }
+
+    IEnumerator DeathPause(){
+        player.GetComponent<playerMove>().isAlive = false;
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("EndLose");
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    // Return to MainMenu
+    public void RestartGame() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        playerHealth = StartPlayerHealth;  // Reset health for new games
+    }
+
+    // Replay the Level where you died
+    public void ReplayLastLevel() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(lastLevelDied);
+        playerHealth = StartPlayerHealth;
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void Credits() {
+        SceneManager.LoadScene("Credits");
+    }
+
 }
