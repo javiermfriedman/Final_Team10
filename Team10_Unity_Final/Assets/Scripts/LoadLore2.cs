218f82a62831714e5e6d1cbb10231a28be0fcdb5
ellavoorhees <ella.voorhees@tufts.edu>
2024-11-25 13:33:48 -0500

    Level 2 pick up totem2

---
+++ Team10_Unity_Final/Assets/Scripts/LoadLore2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TotemSceneSwitch2 : MonoBehaviour
+{
+    // This function is called when another 2D collider enters the trigger collider
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object colliding is the player
+        if (other.CompareTag("Player"))
+        {
+            // Load the specified scene
+            SceneManager.LoadScene("Lore2");
+        }
+    }
+}
