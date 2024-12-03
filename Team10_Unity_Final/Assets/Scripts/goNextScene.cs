d6e300a60233cd63b2c503c72b23e6477fb6865e
nperry02 <nate@atperry.com>
2024-12-03 17:19:30 -0500

    put totem in

---
+++ Team10_Unity_Final/Assets/Scripts/goNextScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class goNextScene : MonoBehaviour
+{
+    public string NextLevel = "SampleScene";
+
+
+
+    // This function is called when another 2D collider enters the trigger collider
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object colliding is the player
+        if (other.CompareTag("Player"))
+        {
+            // Load the specified scene
+            SceneManager.LoadScene(NextLevel);
+        }
+    }
+}
