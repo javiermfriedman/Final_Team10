10bfe7092b999008d68842897072379544bdeb04
nperry02 <nate@atperry.com>
2024-12-03 14:50:53 -0500

    fixed damage

--- Team10_Unity_Final/Assets/Scripts/EnemyMoveHit.cs
+++ Team10_Unity_Final/Assets/Scripts/EnemyMoveHit.cs
@@ -1,123 +1,138 @@
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMoveHit : MonoBehaviour {
+public class EnemyMoveHit : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb2D;
+    public float speed = 4f;
+    private Transform target;
+    public int damage = 10;
+    private float waitTime;
 
-       public Animator anim;
-       public Rigidbody2D rb2D;
-       public float speed = 4f;
-       private Transform target;
-       public int damage = 10;
-       private float waitTime;
+    public int EnemyLives = 1;
+    private GameHandler gameHandler;
 
-       // Make the enemy lives more if the levels are higher --> do this later
-       public int EnemyLives = 1;
-       private GameHandler gameHandler;
+    public float attackRange = 10;
+    public bool isAttacking = false;
+    private float scaleX;
 
-       public float attackRange = 10;
-       public bool isAttacking = false;
-       private float scaleX;
+    public float knockBackForce = 0f;
 
-       public float knockBackForce = 0f;
+    private EnemyMeleeDamage meleeDamage;
+    public SpriteToggle spriteToggle;
 
-       private EnemyMeleeDamage meleeDamage;
-       public SpriteToggle spriteToggle;
-       
-       void Start () {
-              anim = GetComponentInChildren<Animator> ();
-              rb2D = GetComponent<Rigidbody2D> ();
-              scaleX = gameObject.transform.localScale.x;
+    private Coroutine damageCoroutine; // Reference to the damage coroutine
 
-              // Check if Player object is found
-              if (GameObject.FindGameObjectWithTag("Player") != null) {
-                     target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-                     // Debug.LogError("check1");
-              } else {
-              Debug.LogError("Player object with tag 'Player' not found!");
-              }
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+        scaleX = gameObject.transform.localScale.x;
 
-              // Check if GameHandler object is found
-              if (GameObject.FindWithTag("GameHandler") != null) {
-                     gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-                     // gameHandler.testPrint();
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+        else
+        {
+            Debug.LogError("Player object with tag 'Player' not found!");
+        }
 
-              } else {
-              Debug.LogError("GameHandler object with tag 'GameHandler' not found!");
-              }
+        if (GameObject.FindWithTag("GameHandler") != null)
+        {
+            gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+        }
+        else
+        {
+            Debug.LogError("GameHandler object with tag 'GameHandler' not found!");
+        }
 
-              // Check if SpriteToggle component is found
-              spriteToggle = FindObjectOfType<SpriteToggle>();
-              if (spriteToggle == null) {
-              Debug.LogWarning("SpriteToggle component not found! Ghost mode may not work as expected.");
-              }
+        spriteToggle = FindObjectOfType<SpriteToggle>();
+        if (spriteToggle == null)
+        {
+            Debug.LogWarning("SpriteToggle component not found! Ghost mode may not work as expected.");
+        }
+    }
 
-       }
+    void Update()
+    {
+        if (spriteToggle != null && !spriteToggle.isGhostMode)
+        {
+            float DistToPlayer = Vector3.Distance(transform.position, target.position);
+            if ((target != null) && (DistToPlayer <= attackRange))
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
-       void Update () {
-              if (spriteToggle != null && !spriteToggle.isGhostMode)
-              {
-                     float DistToPlayer = Vector3.Distance(transform.position, target.position);
-                     if ((target != null) && (DistToPlayer <= attackRange)){
-                         transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
-                         //anim.SetBool("Walk", true);
-                         //flip enemy to face player direction. Wrong direction? Swap the * -1.
-                     if (target.position.x > gameObject.transform.position.x){
-                         gameObject.transform.localScale = new Vector2(scaleX, gameObject.transform.localScale.y);
-                     } else {
-                         gameObject.transform.localScale = new Vector2(scaleX * -1, gameObject.transform.localScale.y);
-                     }
-                     }
-              }
-              
-              //else { anim.SetBool("Walk", false);}
-       }
+                if (target.position.x > gameObject.transform.position.x)
+                {
+                    gameObject.transform.localScale = new Vector2(scaleX, gameObject.transform.localScale.y);
+                }
+                else
+                {
+                    gameObject.transform.localScale = new Vector2(scaleX * -1, gameObject.transform.localScale.y);
+                }
+            }
+        }
+    }
 
-       public void OnCollisionEnter2D(Collision2D other){
-              if (other.gameObject.tag == "hairBall") {
-                     Debug.Log("hit by hairball");  // Specific log for hairball collision
-                     EnemyLives -= 1;
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "hairBall")
+        {
+            EnemyLives -= 1;
 
-                     if (EnemyLives == 0) {
-                            Destroy(gameObject);
-                     }
-              }
+            if (EnemyLives == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
 
-              if (other.gameObject.tag == "Player") {
-                     Debug.Log("hit by enemy");
-                     isAttacking = true;
+        if (other.gameObject.tag == "Player")
+        {
+            isAttacking = true;
 
-         
-                     //anim.SetBool("Attack", true);
-                     if (gameHandler != null) {
-                            gameHandler.playerGetHit(damage);
-                     } else {
-                            Debug.LogError("GameHandler is not assigned!");
-                     }
+            if (damageCoroutine == null)
+            {
+                damageCoroutine = StartCoroutine(DealDamageOverTime(other.gameObject));
+            }
+        }
+    }
 
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isAttacking = false;
 
-                     //rend.material.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);
-                     //StartCoroutine(HitEnemy());
+            if (damageCoroutine != null)
+            {
+                StopCoroutine(damageCoroutine);
+                damageCoroutine = null;
+            }
+        }
+    }
 
-                     //Tell the player to STOP getting knocked back before getting knocked back:
-                     //other.gameObject.GetComponent<Player_EndKnockBack>().EndKnockBack();
-                     //Add force to the player, pushing them back without teleporting:
-                     //Rigidbody2D pushRB = other.gameObject.GetComponent<Rigidbody2D>();
-                     //Vector2 moveDirectionPush = rb2D.transform.position - other.transform.position;
-                     //pushRB.AddForce(moveDirectionPush.normalized * knockBackForce * - 1f, ForceMode2D.Impulse);
-              }
-       }
+    private IEnumerator DealDamageOverTime(GameObject player)
+    {
+        while (isAttacking)
+        {
+            if (gameHandler != null)
+            {
+                gameHandler.playerGetHit(damage);
+            }
+            else
+            {
+                Debug.LogError("GameHandler is not assigned!");
+            }
 
+            yield return new WaitForSeconds(1f); // Apply damage every second
+        }
+    }
 
-       public void OnCollisionExit2D(Collision2D other){
-              if (other.gameObject.tag == "Player") {
-                     isAttacking = false;
-                     //anim.SetBool("Attack", false);
-              }
-       }
-
-       //DISPLAY the range of enemy's attack when selected in the Editor
-       void OnDrawGizmosSelected(){
-              Gizmos.DrawWireSphere(transform.position, attackRange);
-       }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
 }
